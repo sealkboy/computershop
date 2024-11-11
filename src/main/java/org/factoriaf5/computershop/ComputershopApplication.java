f9f7b31bfc96e2f7c0d4faab7fb01a2ac973701c
unknown <sealkevinboyabad@gmail.com>
2024-11-11 10:49:22 -0800

    Primer commit

---
+++ src/main/java/org/factoriaf5/computershop/ComputershopApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.computershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/computershop/ComputershopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.computershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
